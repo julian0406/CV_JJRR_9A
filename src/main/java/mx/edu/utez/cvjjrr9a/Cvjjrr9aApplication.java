50a88c91a292857195736db3c94a8f80afcd50de
julian0406 <20223tn075@utez.edu.mx>
2025-05-26 17:44:00 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/mx/edu/utez/cvjjrr9a/Cvjjrr9aApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvjjrr9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvjjrr9aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cvjjrr9aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/cvjjrr9a/Cvjjrr9aApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvjjrr9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvjjrr9aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
